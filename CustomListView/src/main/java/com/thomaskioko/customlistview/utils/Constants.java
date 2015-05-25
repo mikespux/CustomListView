12b17f22df8bc5edf2808ceadcb707dddd45685e
mikespux <orengemichael12@gmail.com>
2015-05-25 15:40:26 +0300

    Created Constants class

---
+++ CustomListView/src/main/java/com/thomaskioko/customlistview/utils/Constants.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2015. Thomas Kioko.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.thomaskioko.customlistview.utils;
+
+/**
+ * @author <a href="kiokotomas@gmail.com">Thomas Kioko</a>
+ * @version Version 1.0
+ */
+
+
+public class Constants {
+
+    /**
+     * Movie JSON URL
+     */
+    public static final String URL = "http://api.androidhive.info/json/movies.json";
+}
